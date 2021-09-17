5be4e270fac9dfd6bd14a79f2b65eca1c560a3a2
Lukas Juskevicius <71268640+juskis4@users.noreply.github.com>
2021-09-17 18:07:42 +0000

    More user commands

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ def getCryptoPrices(crypto):
   else:
     return None
 
+def getCryptoChan
 #function to check if crypto is in the database
 def isCryptoSupported(crypto):
   if crypto in db.keys():
@@ -43,19 +44,42 @@ async def on_message(message):
     return 
 
   if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+    embed=discord.Embed(title="Hello! :wave:", description="To get a list of my commands type $help", color=0x109319)
+    await message.channel.send(embed=embed)
 
   #Gets the price of a crypto 
-  if message.content.lower() in db.keys():
-    await message.channel.send(f'The current price of {message.content} is: {getCryptoPrices(message.content.lower())} EUR')
+  if message.content.startswith('$price '):
+    cryptoName = message.content.split('$price ',1)[1].lower()
+    if isCryptoSupported(cryptoName):
+      price = (f'The current price of **{cryptoName.capitalize()}** is: **{getCryptoPrices(cryptoName)}** EUR')
+      embed=discord.Embed(title=cryptoName.capitalize(), description=price, color=0x109319)
+      await message.channel.send(embed=embed)
+    else:
+      await message.channel.send('This crypto may be unsuported! Write $help for further instructions')
+
   #prints the supported crypto list
   if message.content.startswith('$list'):
     cryptoSupportedList = [key for key in db.keys()]
     await message.channel.send(cryptoSupportedList)
+
   #checks if a crypto is supported
   if message.content.startswith('$support '):
     cryptoToBeChecked = message.content.split('$support ',1)[1].lower()
-    await message.channel.send(isCryptoSupported(cryptoToBeChecked))
+    if isCryptoSupported(cryptoToBeChecked):
+      await message.channel.send('Yes I do support this cryptocurrency!')
+    else:
+      await message.channel.send('Sorry but I am not aware of this cryptocurrency')
+  #sends a list of commands
+  if message.content.startswith('$help'):
+    embed=discord.Embed(title="Commands :writing_hand:", description="Here is a list of commands I have:", color=0x109319)
+    
+    embed.add_field(name="$price cryptocurrency", value="Get the current price of a cryptocurrency you typed in", inline=True)
+
+    embed.add_field(name="$list", value="Get a list of cryptocurrencies I support", inline=True)
+
+    embed.add_field(name="$support cryptocurrency", value="Check if I support the cryptocurrency you typed in", inline=False)
+
+    await message.channel.send(embed=embed)
   
             
 
