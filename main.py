87ad211a5d8dec98db09151e1fb6c935a4a7219e
Lukas Juskevicius <71268640+juskis4@users.noreply.github.com>
2021-09-19 15:18:13 +0000

    Upgraded  command to also show 24h change in %

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ import pprint
 from replit import db
 
 
+#function used to get a 24h change percentage of a crypto price
+def get24hChange(crypto):
+  URL = 'https://api.coingecko.com/api/v3/simple/price?ids={}&vs_currencies=eur&include_market_cap=false&include_24hr_vol=false&include_24hr_change=true&include_last_updated_at=false'.format(crypto)
+  r = requests.get(url = URL)
+  data = r.json()
+  data = data[crypto]
+  change = data['eur_24h_change']
+  return round(change, 2)
 
 #getting crypto prices
 def getCryptoPrices(crypto):
   URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur'
   r = requests.get(url = URL)
   data = r.json()
-
   #putting cryptocurrencies with their prices to a database
   for i in range(len(data)):
     db[data[i]['id']] = data[i]['current_price']
-  
   if crypto in db.keys():
     return db[crypto]
   else:
     return None
 
-def getCryptoChan
+
 #function to check if crypto is in the database
 def isCryptoSupported(crypto):
   if crypto in db.keys():
@@ -51,12 +57,22 @@ async def on_message(message):
   if message.content.startswith('$price '):
     cryptoName = message.content.split('$price ',1)[1].lower()
     if isCryptoSupported(cryptoName):
+      change = get24hChange(cryptoName)
+      #Changes the color of a message depending if the price went up or down in 24hours
+      if change > 0:
+        color = 0x1aff00 #green
+      else:
+        color = 0xff0000 #red
+
       price = (f'The current price of **{cryptoName.capitalize()}** is: **{getCryptoPrices(cryptoName)}** EUR')
-      embed=discord.Embed(title=cryptoName.capitalize(), description=price, color=0x109319)
+
+      embed=discord.Embed(title=cryptoName.capitalize(), description=price, color=color)
+      embed.add_field(name ="--------------------------------------------", value="**24 Hour change:** {}%".format(change))
       await message.channel.send(embed=embed)
+
     else:
       await message.channel.send('This crypto may be unsuported! Write $help for further instructions')
-
+  
   #prints the supported crypto list
   if message.content.startswith('$list'):
     cryptoSupportedList = [key for key in db.keys()]
